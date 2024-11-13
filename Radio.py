6975b6e54c4f506d095cbea775641c224c78d12c
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-11-13 18:58:40 +0700

    Update Radio.py

--- Radio.py
+++ Radio.py
@@ -15,7 +15,7 @@ loaded_model = pickle.load(open(filename, "rb"))
 
 background_image = """
 <style>
-[data-testid="_streamlitAppContainer_nim44_1"] > .main {
+[data-testid="stAppViewContainer"] > .main {
     background-image: url("https://img5.pic.in.th/file/secure-sv1/smsk-1e26f337bb6ec6813.jpg");
     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
     background-position: center;
